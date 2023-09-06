1760f086cc46e1f44ad87fbad217789decf5dc5a
dedbih <laslegoaventuras@gmail.com>
2023-09-05 21:51:55 -0600

    Servidor y multiples instancias gui
    todavía no puedo apagar el servidor desde el gui

--- src/main/java/com/example/prueba/HelloApplication.java
+++ src/main/java/com/example/prueba/HelloApplication.java
@@ -3,16 +3,25 @@ package com.example.prueba;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.Socket;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Parent root = fxmlLoader.load();
+
+        // Create a socket and set it in the controller
+        Socket socket = new Socket("localhost", 12345);
+        HelloController controller = fxmlLoader.getController();
+        controller.setSocket(socket);
+
+        Scene scene = new Scene(root, 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/example/prueba/HelloController.java
+++ src/main/java/com/example/prueba/HelloController.java
@@ -3,12 +3,50 @@ package com.example.prueba;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class HelloController {
     @FXML
     private Label welcomeText;
 
+    private Socket socket;
+
+    public void setSocket(Socket socket) {
+        this.socket = socket;
+    }
+
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        String message = "Welcome to JavaFX Application!";
+        // Send the message to the server
+        try {
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            out.println(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Handle messages received from the server and update the view
+    public void handleMessage(String message) {
+        Platform.runLater(() -> {
+            welcomeText.setText(message);
+        });
+    }
+
+    // Method to stop the server
+    public void stopServer() {
+        String stopCommand = "STOP_SERVER";
+        try {
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            out.println(stopCommand);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
