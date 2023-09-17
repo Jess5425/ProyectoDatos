4949a5ae70f924b63c800dfdfa2acf8faf43fd33
Jéssica Jiménez Ureña <jesjimenez@estudiantec.cr>
2023-09-16 20:35:54 -0600

    Clases

---
+++ src/main/java/com/example/prueba/classlista.java
@@ -0,0 +1,50 @@
+public class ListaE {
+    Nodo head;
+
+    public ListaE() {
+        this.head = null;
+    }
+
+    public void insert(int data) {
+        Nodo newNode = new Nodo(data);
+
+        if (head == null) {
+            head = newNode;
+        } else {
+            Nodo current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    public void delete(int data) {
+        if (head == null) {
+            return;
+        }
+
+        if (head.data == data) {
+            head = head.next;
+            return;
+        }
+
+        Nodo current = head;
+        while (current.next != null && current.next.data != data) {
+            current = current.next;
+        }
+
+        if (current.next != null) {
+            current.next = current.next.next;
+        }
+    }
+
+    public void display() {
+        Nodo current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+}
