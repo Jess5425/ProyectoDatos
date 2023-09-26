b9d1f40877eb407d84a0bc2e59e9e20f2d027e20
Jéssica Jiménez Ureña <jesjimenez@estudiantec.cr>
2023-09-25 22:44:05 -0600

    Servidor agregado

---
+++ src/Servidor/MainServer.java
@@ -0,0 +1,10 @@
+package Servidor;
+
+import Cliente.VentanaJuego;
+import Servidor.Conexion.Servidor;
+
+public class MainServer {
+    public static void main(String[] args) {
+        Servidor servidor = new Servidor();
+    }
+}
