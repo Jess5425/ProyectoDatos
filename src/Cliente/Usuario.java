f1bd24fc9a5b45f641f39e599b263d7e3c7a7de3
Jéssica Jiménez Ureña <jesjimenez@estudiantec.cr>
2023-09-25 22:53:04 -0600

    Estructura usuario

--- src/Cliente/Usuario.java
+++ src/Cliente/Usuario.java
@@ -1,6 +1,11 @@
 package Cliente;
 
 public class Usuario {
-   
+    public int puerto;
+    public String nombre;
+    public Usuario(){}
+    public Usuario(int p, String n){
+        this.puerto=p;
+        this.nombre=n;
     }
-
+}
