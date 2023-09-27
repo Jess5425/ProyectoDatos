37b433909c217690b48090c536165e2182abf258
LiaV11 <142848311+LiaV11@users.noreply.github.com>
2023-09-27 04:47:36 -0600

    Update Instrucciones.java

---
+++ src/Cliente/Instrucciones.java
@@ -0,0 +1,11 @@
+package Cliente;
+
+public class Instrucciones {
+    public String instruccion;
+    public String datos;
+
+    public Instrucciones(String instruccion, String datos) {
+        this.instruccion = instruccion;
+        this.datos = datos;
+    }
+}
