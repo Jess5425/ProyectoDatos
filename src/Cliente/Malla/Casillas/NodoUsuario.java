c9935948472dfd1c33c5c79ca80b49270790cb36
Jéssica Jiménez Ureña <jesjimenez@estudiantec.cr>
2023-09-29 13:11:21 -0600

    Aqui se almacenan los usuarios

--- src/Cliente/Malla/Casillas/NodoUsuario.java
+++ src/Cliente/Malla/Casillas/NodoUsuario.java
@@ -1,3 +1,16 @@
+package Cliente.Malla.Casillas;
+
+import Cliente.Usuario;
+/***
+ * Nodo donde se alamacenan los usuarios
+ */
 public class NodoUsuario {
-    
+    public Usuario data;
+    public NodoUsuario next;
+
+    /***
+     * Metodo constructor
+     */
+    public NodoUsuario() {
+    }
 }
