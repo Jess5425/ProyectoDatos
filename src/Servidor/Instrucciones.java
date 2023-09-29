33cdd5c994e238c3e67972a7691d17f77f4f2358
dedbih <laslegoaventuras@gmail.com>
2023-09-29 17:15:38 -0600

    MainServer e instrucciones

---
+++ src/Servidor/Instrucciones.java
@@ -0,0 +1,23 @@
+package Servidor;
+/***
+ * Clase que se utiliza para enviar instrucciones al servidor
+ */
+public class Instrucciones {
+    public String instruccion;
+    public String datos;
+
+    /**
+     * Metodo constructor
+     */
+    public Instrucciones() {
+    }
+    /***
+     * Metodo constructor
+     * @param instruccion instruccion que se envia al servidor
+     * @param datos datos adicionales para ejecutar la accion
+     */
+    public Instrucciones(String instruccion, String datos) {
+        this.instruccion = instruccion;
+        this.datos = datos;
+    }
+}
---
+++ src/Servidor/MainServer.java
@@ -0,0 +1,9 @@
+package Servidor;
+
+import Servidor.Conexion.Servidor;
+
+public class MainServer {
+    public static void main(String[] args) {
+        Servidor servidor = new Servidor();
+    }
+}
