7d5a89606bdd17465fb14d7961f9754d5460c941
Jéssica Jiménez Ureña <jesjimenez@estudiantec.cr>
2023-09-29 13:00:08 -0600

    Comentarios del código

--- src/Cliente/Usuario.java
+++ src/Cliente/Usuario.java
@@ -1,9 +1,18 @@
 package Cliente;
 
+/***
+ * Clase Usuario
+ */
 public class Usuario {
     public int puerto;
     public String nombre;
     public Usuario(){}
+
+    /***
+     * Metodo constructor
+     * @param p puerto
+     * @param n nombre del usuario
+     */
     public Usuario(int p, String n){
         this.puerto=p;
         this.nombre=n;
