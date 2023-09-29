c7b4dadeba2038be81198153a52f16618c6a6546
Jéssica Jiménez Ureña <jesjimenez@estudiantec.cr>
2023-09-29 00:51:02 -0600

    user

---
+++ src/Servidor/Jugadores/Usuario.java
@@ -0,0 +1,11 @@
+package Servidor.Jugadores;
+
+public class Usuario {
+    public int puerto;
+    public String nombre;
+    public Usuario(){}
+    public Usuario(int p, String n){
+        this.puerto=p;
+        this.nombre=n;
+    }
+}
